072727086f2cffdd3a78dbc504087da23bcdd104
Haris Mohammad Shah <theharisshah@users.noreply.github.com>
2018-01-31 10:22:22 +0530

    Update search.blade.php

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -3,7 +3,7 @@
   <head>
     <title>Title</title>
     <meta charset="utf-8">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+   
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.css" />
@@ -33,11 +33,7 @@ $( function() {
   $( "#SearchItem" ).autocomplete({
     source: "{{ route('search') }}"
 });
-$.ajaxSetup({
-  headers: {
-    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-  }
-});
+
   $("#SearchItem").keypress(function(e) {
    
     if(e.keyCode == 13){
