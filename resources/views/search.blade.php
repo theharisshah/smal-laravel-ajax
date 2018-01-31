6d812bf898eb836c0c0e48c4917742462f439535
Haris Mohammad Shah <theharisshah@users.noreply.github.com>
2018-01-31 11:24:08 +0530

    Update search.blade.php

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -4,21 +4,23 @@
     <title>MobilePhoneLookUp</title>
     <meta charset="utf-8">
    
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.css" />
     <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
-    <div class="container">
+    <div class="container-fluid">
       <!-- Search Bar -->
    
       <div class="search mx-auto" style="width: 35%; margin-top:100px;">
         <input type="text" class="form-control" name="SearchItem" id="SearchItem" placeholder="Search..." >
+        </div>
       <!-- results displayed here -->
       <div class="results" id="results" style="margin-top:40px;">
   
         
+    </div>
     </div>
     <!-- all scripts  -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
