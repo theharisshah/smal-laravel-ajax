ac34d1fb7dac1873288cb04c49a846374983c5ef
Haris Mohammad Shah <theharisshah@users.noreply.github.com>
2018-01-31 12:41:46 +0530

    Update search.blade.php

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -23,7 +23,7 @@
     </div>
     </div>
     <!-- all scripts  -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+   
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
     <script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
