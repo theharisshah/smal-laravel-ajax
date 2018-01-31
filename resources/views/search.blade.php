4b5ce27afd613fd0a18dca10e291e07ad4e149ed
Haris Mohammad Shah <theharisshah@users.noreply.github.com>
2018-01-31 10:56:47 +0530

    changed title

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <title>Title</title>
+    <title>MobilePhoneLookUp</title>
     <meta charset="utf-8">
    
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
