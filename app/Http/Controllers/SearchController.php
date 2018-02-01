cd9621c48bb818f9542990472b8f127c8b1d7d16
Haris Mohammad Shah <theharisshah@users.noreply.github.com>
2018-02-02 00:05:43 +0530

    Update SearchController.php

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -21,54 +21,20 @@ class SearchController extends Controller
         }
         return $searchResult;
     }
-    public function view(Request $request){ 
-        if($request->ajax()){
-            $results = "";
-            $term = $request->input('SearchItem');
-            
-          $items = Search::where('PhoneName', 'LIKE', $term.'%')->first();
-
-                    $results.='
-                    <div class="card-header">
-                        <ul class="nav nav-pills" role="tablist">
-                            <li class="nav-item">
-                                <a class="nav-link active" role="tab" data-toggle="tab" href="#Overview">Overview</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" role="tab" data-toggle="tab" href="#Specs">Specifications</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="tab-content">
-                        <div role="tabpanel" class="tab-pane active" id="Overview">
-                            <img class="card-img-bottom" src='.$items->url.'> 
-                                <div class="card-body"> 
-                                    <h5 class="card-title">'.$items->PhoneName.'</h5> 
-                                    <p  class="card-text">'.$items->Overview.'</p>
-                                </div>
-                        </div>
-                        <div role="tabpanel" class="tab-pane" id="Specs">
-                            <ul class="list-group">
-                                <li class="list-group-item d-flex justify-content-between align-items-center">
-                                    Operating System: '.$items->os.'
-                                </li>
-                                <li class="list-group-item d-flex justify-content-between align-items-center">
-                                    Memory: '.$items->Ram.'
-                                 </li>
-                                <li class="list-group-item d-flex justify-content-between align-items-center">
-                                    Camera: '.$items->Camera.'
-                                </li>
-                                <li class="list-group-item d-flex justify-content-between align-items-center">
-                                    Display: '.$items->Display.'
-                                </li>
-                            </ul>
-                        </div>
-                     </div>';
-              
-        
-            return response($results);
-        }
+    public function phone(Request $request){
+        // if($request->ajax()){
+        $term = $request->input('SearchItem');
+          
+        $items = Search::where('PhoneName', 'LIKE', $term.'%')->first();
+        $url = $items->url;
+        $phonename=$items->PhoneName;
+        $overview = $items->Overview;
+        $os = $items->os;
+        $ram =$items->Ram;
+        $camera = $items->Camera;
+        $display = $items->Display;
+        return view('phoneresults', compact('url','phonename','overview','os','ram','camera','display'));
+}
         
 
-    }
 }
