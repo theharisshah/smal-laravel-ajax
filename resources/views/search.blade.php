a1829b0b7947405a5dfd2cc48eb9f3ab0c099a23
Haris Mohammad Shah <theharisshah@users.noreply.github.com>
2018-02-02 00:14:42 +0530

    Add files via upload

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,42 +1,33 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <title>MobilePhoneLookUp</title>
+    <title>Title</title>
     <meta charset="utf-8">
-   
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.css" />
-   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
-   <link rel="stylesheet" href="css/style.css">
-   
-    
+    <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
-    <div class="container-fluid">
+    <div class="container">
       <!-- Search Bar -->
-       <form action="/phone" method="get" id="aa">
+      <form action="/phone" method="get" id="aa">
       <div class="search mx-auto" style="width: 35%; margin-top:100px;">
         <input type="text" class="form-control" name="SearchItem" id="SearchItem" placeholder="Search..." >
-        </div>
         </form>
       <!-- results displayed here -->
       <div class="results" id="results" style="margin-top:40px;">
-       @yield('content')
+          @yield('content')
         
-    </div>
-    </div>
+      </div>
     <!-- all scripts  -->
-    <script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
-    
-    
+    <script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.js"></script>
     <script src="js/main.js"></script>
-  <script type='text/javascript'>
-$( function() {
-  
-  
+  <script>
 $( function abc() {
   //$('#results').hide();
   $( "#SearchItem" ).autocomplete({
@@ -46,6 +37,9 @@ $( function abc() {
     }
 });
 
+
+
+});
   </script>
   </body>
 </html>
